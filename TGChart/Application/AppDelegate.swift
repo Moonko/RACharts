74cd371b67e0176535730ff65d3124a5bf661af5
Andrei Rychkov <rychkov.dev@gmail.com>
2019-10-19 13:59:05 +0300

    Support iOS 13 theme change

--- TGChart/Application/AppDelegate.swift
+++ TGChart/Application/AppDelegate.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        window = UIWindow()
+        window = AppWindow()
         let chartsListController = ChartsListTableViewController()
         let navController = ThemedNavigationController(rootViewController: chartsListController)
         navController.statusBarStylePalette = Palette.statusBarStyle
@@ -15,6 +15,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
 
         return true
+    }
+}
+
+final class AppWindow: UIWindow {
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+
+        guard #available(iOS 13, *) else { return }
+
+        switch traitCollection.userInterfaceStyle {
+        case .dark where ThemeSelector.themeIndex == 0,
+             .light where ThemeSelector.themeIndex == 1:
+            ThemeSelector.chooseNextTheme()
+        default:
+            return
+        }
     }
 }
 
